d1e10173d47cb63b1f0496fc73359feb77d20341
Taps73r <povroznykmisha@gmail.com>
2024-03-09 13:49:24 +0200

    Add routes

--- teacher-project/src/constants/routes.ts
+++ teacher-project/src/constants/routes.ts
@@ -1,3 +1,12 @@
 export const ROUTES = {
     LOGIN: "/login",
+    REGISTRATION: "/registration",
+    TEACHER_SCORES: "/teachers-scores",
+    MY_RESULTS: "/my-results",
+    SUBMIT_TASK: "/submit-task",
+    COMMITTE_TEACHERS: "/committee-teachers",
+    ADD_TEACHER: "/add-teacher",
+    TEACHER_REQUESTS: "/teacher-requests",
+    ALL_USERS: "/all-users",
+    MANAGE_TEACHERS: "/manage-teachers",
 };
