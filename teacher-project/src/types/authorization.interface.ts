f1b07823545f9f0dadccedaf48610f096ba83552
Taps73r <povroznykmisha@gmail.com>
2024-03-12 23:10:36 +0200

    add interfaces

---
+++ teacher-project/src/types/authorization.interface.ts
@@ -0,0 +1,20 @@
+export interface IRegisterFields {
+    email: string;
+    password: string;
+}
+
+export interface ILoginFields {
+    email: string;
+    password: string;
+}
+
+export interface ICredentials {
+    email: string;
+    password: string;
+}
+
+export interface IUserData {
+    name: string;
+    email: string;
+    password: string;
+}
