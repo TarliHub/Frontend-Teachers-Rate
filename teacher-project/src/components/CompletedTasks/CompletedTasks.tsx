825607248d0747d96c8c208a02206735f1e58342
Taps73r <povroznykmisha@gmail.com>
2024-04-11 14:50:47 +0300

    add completedTasks files

--- teacher-project/src/components/CompletedTasks/CompletedTasks.tsx
+++ teacher-project/src/components/CompletedTasks/CompletedTasks.tsx
@@ -1,5 +1,16 @@
+import { fakeTasks } from "../../constants/fakeData";
 import styles from "./CompletedTasks.module.scss";
 
 export function CompletedTasks(): JSX.Element {
-    return <div></div>;
+    return (
+        <div className={styles.taskContainer}>
+            {fakeTasks.map((item) => (
+                <div className={styles.taskElement} key={item.id}>
+                    <p>{item.name}</p>
+                    <p>{item.description}</p>
+                    <p>Оцінка: {item.grade}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
