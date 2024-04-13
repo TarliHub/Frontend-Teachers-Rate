f118c1554cf27fe54ef2992b71b9b4ec9052e5b9
Taps73r <povroznykmisha@gmail.com>
2024-04-14 01:17:11 +0300

    delete inputError component

--- teacher-project/src/components/InputError/InputError.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./InputError.module.scss";
-
-interface IInputErrorProps {
-    errorMessage?: string;
-}
-
-export function InputError({ errorMessage }: IInputErrorProps): JSX.Element {
-    return <div className={styles.errorText}>{errorMessage}</div>;
-}
