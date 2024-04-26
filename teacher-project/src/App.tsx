dd9beec49b6ea11d263fb0f65ac105b754aa809c
Taps73r <povroznykmisha@gmail.com>
2024-04-26 22:59:30 +0300

    import to app tsx page

--- teacher-project/src/App.tsx
+++ teacher-project/src/App.tsx
@@ -7,12 +7,14 @@ import { ROUTES } from "./constants/routes";
 import { Registration } from "./pages/Registration";
 import { Login } from "./pages/Login";
 import { Main } from "./pages/Main";
+import { Tasks } from "./pages/Tasks";
 
 function App(): JSX.Element {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<Main />} path={ROUTES.MAIN} />
+                <Route element={<Tasks />} path={ROUTES.TASKS} />
                 <Route element={<Login />} path={ROUTES.LOGIN} />
                 <Route element={<Registration />} path={ROUTES.REGISTRATION} />
             </Routes>
