3cec32858cc47905f71405958818d8ac796060d1
Taps73r <povroznykmisha@gmail.com>
2024-04-27 23:55:44 +0300

    adding mobile styles

--- teacher-project/src/components/NavigationBar/NavigationBarBlock.tsx
+++ teacher-project/src/components/NavigationBar/NavigationBarBlock.tsx
@@ -1,7 +1,10 @@
 import styles from "./NavigationBar.module.scss";
 import { CustomNavLink } from "./CustomNavLink/CustomNavLink";
 import { CollegeLogo } from "../CollegeLogo/CollegeLogo";
+import menuIcon from "../../assets/icons/mobileMenuIcon.svg";
 import { INavigation } from "../../types/Navigation";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 interface INavigationBarBlockProps {
     navigationData: INavigation[];
@@ -10,12 +13,39 @@ interface INavigationBarBlockProps {
 export function NavigationBarBlock({
     navigationData,
 }: INavigationBarBlockProps): JSX.Element {
+    const [showMenu, setShowMenu] = useState<boolean>(false);
+
     return (
-        <div className={styles.navigationBar}>
-            <CollegeLogo />
-            {navigationData.map((item, index) => (
-                <CustomNavLink key={index} url={item.url} name={item.name} />
-            ))}
-        </div>
+        <>
+            <div className={styles.mobileMenu}>
+                <div className={styles.listOfButtons}>
+                    <h2>Меню</h2>
+                    {navigationData.map((item, index) => (
+                        <Link key={index} to={item.url}>
+                            {item.name}
+                        </Link>
+                    ))}
+                </div>
+            </div>
+            <div className={styles.navigationBar}>
+                <CollegeLogo />
+                {navigationData.map((item, index) => (
+                    <CustomNavLink
+                        key={index}
+                        url={item.url}
+                        name={item.name}
+                    />
+                ))}
+                <div className={styles.mobileMenuButton}>
+                    <button onClick={() => setShowMenu(!showMenu)}>
+                        <img
+                            className="max-w-10 max-h-10"
+                            src={menuIcon}
+                            alt="Menu Icon"
+                        />
+                    </button>
+                </div>
+            </div>
+        </>
     );
 }
