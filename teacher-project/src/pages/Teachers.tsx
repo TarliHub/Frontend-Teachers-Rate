e03b55a08df6f4a880f68ab15677717ee4c9de73
Taps73r <povroznykmisha@gmail.com>
2024-05-08 12:59:09 +0300

    update create button

--- teacher-project/src/pages/Teachers.tsx
+++ teacher-project/src/pages/Teachers.tsx
@@ -8,9 +8,15 @@ import { usersData } from "../constants/fakeData";
 export function Teachers(): JSX.Element {
     return (
         <div>
-            <div>
-                <Link to={`${ROUTES.TEACHERS}/create-user`}>
-                    Створити користувача
+            <div className="flex justify-end m-4">
+                <Link
+                    className=" flex items-center gap-1 text-secondaryBlue cursor-pointer"
+                    to={`${ROUTES.TEACHERS}${ROUTES.CREATE_USER}`}
+                >
+                    <span className="material-symbols-outlined text-3xl">
+                        add
+                    </span>
+                    <p className="text-lg font-medium">СТВОРИТИ</p>
                 </Link>
             </div>
             <div className="flex flex-row">
