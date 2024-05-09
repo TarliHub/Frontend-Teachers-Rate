63986dcc37249626647aa4443512076c4be373be
Taps73r <povroznykmisha@gmail.com>
2024-05-09 17:20:42 +0300

    add AutchContext with hook, reducer, provider

--- teacher-project/src/main.tsx
+++ teacher-project/src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthContextProvider } from "./context/AuthContext.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
+        <AuthContextProvider>
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
+        </AuthContextProvider>
     </React.StrictMode>
 );
