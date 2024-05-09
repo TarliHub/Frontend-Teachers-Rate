2b0569b13cc73081d4677376bb726fb8efb8b215
Taps73r <povroznykmisha@gmail.com>
2024-05-10 00:44:18 +0300

    update app if user not login

--- teacher-project/src/App.tsx
+++ teacher-project/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.scss";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import { ROUTES } from "./constants/routes";
 
@@ -13,25 +13,37 @@ import { Teachers } from "./pages/Teachers";
 import { CreateUser } from "./pages/CreateUser";
 import { UpdateUser } from "./pages/UpdateUser";
 
+import { useIsLogin } from "./hooks/useIsLogin";
+
 function App(): JSX.Element {
+    const isLogin = useIsLogin();
+
     return (
         <BrowserRouter>
-            <NavigationBar role="Admin" />
-            <Routes>
-                <Route element={<Main />} path={ROUTES.MAIN} />
-                <Route element={<Tasks />} path={ROUTES.TASKS} />
-                <Route element={<Profile />} path={ROUTES.PROFILE} />
-                <Route element={<Login />} path={ROUTES.LOGIN} />
-                <Route element={<Teachers />} path={ROUTES.TEACHERS} />
-                <Route
-                    element={<CreateUser />}
-                    path={`${ROUTES.TEACHERS}${ROUTES.CREATE_USER}`}
-                />
-                <Route
-                    element={<UpdateUser />}
-                    path={`${ROUTES.TEACHERS}${ROUTES.UPDATE_USER}/:id`}
-                />
-            </Routes>
+            {isLogin ? (
+                <>
+                    <NavigationBar role="Admin" />
+                    <Routes>
+                        <Route element={<Main />} path={ROUTES.MAIN} />
+                        <Route element={<Tasks />} path={ROUTES.TASKS} />
+                        <Route element={<Profile />} path={ROUTES.PROFILE} />
+                        <Route element={<Teachers />} path={ROUTES.TEACHERS} />
+                        <Route
+                            element={<CreateUser />}
+                            path={`${ROUTES.TEACHERS}${ROUTES.CREATE_USER}`}
+                        />
+                        <Route
+                            element={<UpdateUser />}
+                            path={`${ROUTES.TEACHERS}${ROUTES.UPDATE_USER}/:id`}
+                        />
+                    </Routes>
+                </>
+            ) : (
+                <Routes>
+                    <Route element={<Login />} path={ROUTES.LOGIN} />
+                    <Route path="*" element={<Navigate to={ROUTES.LOGIN} />} />
+                </Routes>
+            )}
         </BrowserRouter>
     );
 }
