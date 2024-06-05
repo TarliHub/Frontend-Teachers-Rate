da5e0c59acb16faf1f8e247a42ef41f6ff8c4ad3
Taps73r <povroznykmisha@gmail.com>
2024-06-05 13:50:09 +0300

    fix

--- teacher-project/src/pages/Task.tsx
+++ teacher-project/src/pages/Task.tsx
@@ -45,16 +45,6 @@ export function Task() {
         <div className="flex flex-col text-2xl p-3 pb-5 border-b-[2px] border-slate-300 gap-4">
             <h1 className="text-3xl font-bold">{data?.title}</h1>
             <p className="text-lg">Опис: {data?.pointsDescription}</p>
-            <div className="mb-4">
-                <h2 className="text-2xl font-semibold mb-2">Оцінки</h2>
-                <ul className="flex flex-row text-xl gap-2 pl-5">
-                    {data?.points.map((point, index) => (
-                        <li key={index} className="mb-1">
-                            {point}
-                        </li>
-                    ))}
-                </ul>
-            </div>
             <p className="text-lg mb-4">Підтвердження: {data?.approval}</p>
             <div>
                 <h2 className="text-2xl font-semibold mb-2">Категорія</h2>
